7c6f2b8f9ebc233c553620a859bf6f973de670f5
K-Pomian <pomian.konrad@gmail.com>
2023-02-26 19:51:08 +0100

    Fix contract ts file

--- sdk/src/contract.ts
+++ sdk/src/contract.ts
@@ -1,22 +1,34 @@
-import { AptosClient, Types } from 'aptos'
-import { Wallet } from './wallet'
+import { AptosClient, Types, AptosAccount } from 'aptos'
+import { Network } from './network'
+import { DEVNET_ADMIN_PRIV_KEY, DEVNET_CONTRACT_ADDRESS, DEVNET_NODE_URL } from './static'
+import { TestWallet, Wallet } from './wallet'
 
 export class Contract {
   public aptosClient: AptosClient
   public contractAddress: string
   public wallet: Wallet
 
+  public constructor(network: Network) {
+    switch (network) {
+      case Network.DEVNET: {
+        this.aptosClient = new AptosClient(DEVNET_NODE_URL)
+        this.contractAddress = DEVNET_CONTRACT_ADDRESS
+        this.wallet = new TestWallet(new AptosAccount(DEVNET_ADMIN_PRIV_KEY), this.aptosClient)
+      }
+    }
+  }
+
   public async createGame(
-    coin_type: string,
-    game_name: string,
+    coinType: string,
+    gameName: string,
     amountPerDepositor: number,
     withdrawalFractions: number[],
     joinDuration: number
   ) {
     return await this.signAndSend(
       await this.createGamePayload(
-        coin_type,
-        game_name,
+        coinType,
+        gameName,
         amountPerDepositor,
         withdrawalFractions,
         joinDuration
@@ -25,17 +37,17 @@ export class Contract {
   }
 
   public async createGamePayload(
-    coin_type: string,
-    game_name: string,
+    coinType: string,
+    gameName: string,
     amountPerDepositor: number,
     withdrawalFractions: number[],
     joinDuration: number
   ) {
     const entryFunctionPayload: Types.TransactionPayload_EntryFunctionPayload = {
       type: 'entry_function_payload',
-      function: `${this.contractAddress}::core::create_game`,
-      type_arguments: [coin_type],
-      arguments: [game_name, amountPerDepositor, withdrawalFractions, joinDuration]
+      function: `0x${this.contractAddress}::core::create_game`,
+      type_arguments: [coinType],
+      arguments: [gameName, amountPerDepositor, withdrawalFractions, joinDuration]
     }
 
     return entryFunctionPayload
